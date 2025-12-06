e617a87a3c18e1f8ed4d6319a5703073fbf0a9c8
Shunsuke Suzuki <suzuki-shunsuke@users.noreply.github.com>
2025-12-06 23:35:30 +0900

    fix: update help message (#377)

--- pkg/cli/runner.go
+++ pkg/cli/runner.go
@@ -3,7 +3,6 @@ package cli
 import (
 	"fmt"
 	"io"
-	"log/slog"
 
 	"github.com/spf13/afero"
 	flag "github.com/spf13/pflag"
@@ -21,24 +20,24 @@ files: GitHub Actions files. By default, \.github/workflows/*\.ya?ml$
 
 Options:
 	--help, -h       Show help
-	--version, -v    Show version`
+	--version, -v    Show version
+	--log-level      The log level (default: info, debug, info, warn, error)
+	--log-color      The log color (always, never, auto)
+	--dry-run        Dry Run`
 
 type Runner struct {
-	Stdin       io.Reader
-	Stdout      io.Writer
-	Stderr      io.Writer
-	LDFlags     *LDFlags
-	Logger      *slog.Logger
-	LogLevelVar *slog.LevelVar
+	Stdin   io.Reader
+	Stdout  io.Writer
+	Stderr  io.Writer
+	LDFlags *LDFlags
+	Logger  *slogutil.Logger
 }
 
 type LDFlags struct {
 	Version string
-	Commit  string
-	Date    string
 }
 
-func (r *Runner) Run() error {
+func (r *Runner) Run(logger *slogutil.Logger) error {
 	flg := &Flag{}
 	parseFlags(flg)
 	if flg.Version {
@@ -49,13 +48,13 @@ func (r *Runner) Run() error {
 		fmt.Fprintln(r.Stdout, help)
 		return nil
 	}
-	if err := slogutil.SetLevel(r.LogLevelVar, flg.LogLevel); err != nil {
+	if err := logger.SetLevel(flg.LogLevel); err != nil {
 		return fmt.Errorf("set log level: %w", err)
 	}
 
 	ctrl := &controller.Controller{}
 	ctrl.Init(afero.NewOsFs(), r.Stdout, r.Stderr)
-	return ctrl.Run(r.Logger, &controller.Input{ //nolint:wrapcheck
+	return ctrl.Run(logger.Logger, &controller.Input{ //nolint:wrapcheck
 		DryRun: flg.DryRun,
 		Args:   flg.Args,
 	})
@@ -63,6 +62,7 @@ func (r *Runner) Run() error {
 
 type Flag struct {
 	LogLevel string
+	LogColor string
 	Args     []string
 	Help     bool
 	Version  bool
@@ -71,6 +71,7 @@ type Flag struct {
 
 func parseFlags(f *Flag) {
 	flag.StringVar(&f.LogLevel, "log-level", "info", "The log level")
+	flag.StringVar(&f.LogColor, "log-color", "auto", "The log color")
 	flag.BoolVarP(&f.Help, "help", "h", false, "Show help")
 	flag.BoolVarP(&f.Version, "version", "v", false, "Show version")
 	flag.BoolVar(&f.DryRun, "dry-run", false, "Dry Run")
